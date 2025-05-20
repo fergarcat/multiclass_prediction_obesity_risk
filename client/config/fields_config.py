0db4e7cb073206ba4ebf1cdd113bce7b0d40d07b
a-bac-0 <anca.bacria@thevallians.com>
2025-05-20 09:40:46 +0200

    feature: created fields for gender, age and family history as a config for a modular use Co-authored-by: fergarcat <fernandogcatalan@gmail.com> Co-authored-by: abbyenredes <abbyenredes@gmail.com> Co-authored-by: Omarlsant <omarns21@gmail.com>

--- client/config/fields_config.py
+++ client/config/fields_config.py
@@ -10,16 +10,6 @@ FIELDS = {
         "min": 0,
         "max": 120
     },
-    "Height": {
-        "type": "numeric",
-        "label": "Height (m)",
-        "step": 0.01
-    },
-    "Weight": {
-        "type": "numeric",
-        "label": "Weight (kg)",
-        "step": 0.1
-    },
     "family_history_with_overweight": {
         "type": "radio",
         "label": "Family History of Overweight",
