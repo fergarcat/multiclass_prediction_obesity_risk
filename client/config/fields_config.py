10277edf359a52676fa1aef42b7bf8f9782be7ac
a-bac-0 <anca.bacria@thevallians.com>
2025-05-20 08:05:08 +0200

    feature: created fields for gender, age and family history as a config for a modular use

---
+++ client/config/fields_config.py
@@ -0,0 +1,29 @@
+FIELDS = {
+    "Gender": {
+        "type": "dropdown",
+        "label": "Gender",
+        "options": ["Male", "Female"]
+    },
+    "Age": {
+        "type": "numeric",
+        "label": "Age (years)",
+        "min": 0,
+        "max": 120
+    },
+    "Height": {
+        "type": "numeric",
+        "label": "Height (m)",
+        "step": 0.01
+    },
+    "Weight": {
+        "type": "numeric",
+        "label": "Weight (kg)",
+        "step": 0.1
+    },
+    "family_history_with_overweight": {
+        "type": "radio",
+        "label": "Family History of Overweight",
+        "options": ["yes", "no"]
+    },
+    
+}
