927bd9b7d15687331b382dfbabb15328d1893f05
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-05-21 12:26:51 +0200

    feat(benchmarks): modify input parameters
    X = df[features]
    y = df[target]

--- data/modeling/benchmarks.py
+++ data/modeling/benchmarks.py
@@ -18,9 +18,8 @@ from catboost import CatBoostClassifier
 
 from tabulate import tabulate  # pip install tabulate
 
-def ml_benchmarks(df, features, target, test_size=0.2, random_state=42):
-    X = df[features]
-    y = df[target]
+
+def ml_benchmarks(X, y, test_size=0.2, random_state=42):
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=random_state, stratify=y
