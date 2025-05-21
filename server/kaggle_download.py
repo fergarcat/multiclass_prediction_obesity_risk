cbdb2472571317df93cc8f437353edbe674c226f
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-05-21 09:47:51 +0200

    feat(kaggle_download): parameters change
    change default parameters to be used directly from notebook

--- server/kaggle_download.py
+++ server/kaggle_download.py
@@ -1,8 +1,8 @@
 import os
 import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
-
-def download_kaggle_competition(competition_name: str, destination: str = "../data/.kaggle", unzip: bool = True):
+destination_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", ".kaggle"))
+def download_kaggle_competition(competition_name: str="playground-series-s4e2", destination: str= destination_path, unzip: bool = True):
     """
     Downloads files from a Kaggle competition.
 
@@ -12,7 +12,8 @@ def download_kaggle_competition(competition_name: str, destination: str = "../da
     - unzip: whether to automatically unzip the downloaded files (default is True)
     """
     # Set environment variable to locate kaggle.json credentials
-    os.environ["KAGGLE_CONFIG_DIR"] = os.path.expanduser("~/.kaggle")
+
+    os.environ["KAGGLE_CONFIG_DIR"] = os.path.join(os.path.expanduser("~"), ".kaggle")
 
     # Initialize Kaggle API client
     api = KaggleApi()
@@ -38,4 +39,7 @@ def download_kaggle_competition(competition_name: str, destination: str = "../da
 
 # Example usage
 if __name__ == "__main__":
-    download_kaggle_competition("playground-series-s4e2")
+    print("Running Kaggle Download from main()...")
+    # Cambia a ruta absoluta de tu proyecto si quieres:
+    print(f"Destination path: {destination_path}")
+    download_kaggle_competition(destination = destination_path)
